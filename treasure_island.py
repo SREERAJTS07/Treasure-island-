fd5382d17063ed9ac43a626cd2c45557507cc496
SREERAJ T SURENDRAN <142571237+SREERAJTS07@users.noreply.github.com>
2023-12-24 11:43:41 +0530

    treasure_island.py

--- treasure_island.py
+++ treasure_island.py
@@ -1,24 +1,30 @@
-print("Welcome to treasure Island. \n"
-      "Your mission is to find the treasure.")
-choice1 = input("You\'re at a crossroad, where do you want to go? Type 'left ' or 'right'. \n").lower()
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+
+# Asking for user input for the first decision
+choice1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right': ").lower()
+
 if choice1 == "left":
-    print("You are still alive")
-    choice2 = input("You\'ve come to a lake. There is an island in the middle of the lake."
-                    " Type 'wait' to wait for a boat. Type 'swim' to swim across\n").lower()
+    print("You are still alive.")
+    # Asking for user input for the second decision
+    choice2 = input("You've come to a lake. There is an island in the middle of the lake. "
+                    "Type 'wait' to wait for a boat or 'swim' to swim across: ").lower()
+
     if choice2 == "wait":
-        print('You are still alive.')
-        choice3  = input("You arrive at the island unharmed."
-                         "There is a house with 3 door. One red, One yellow and One blue\n").lower()
+        print("You are still alive.")
+        # Asking for user input for the third decision
+        choice3 = input("You arrive at the island unharmed. "
+                        "There is a house with 3 doors: one red, one yellow, and one blue. "
+                        "Which door do you choose? ").lower()
+
         if choice3 == "yellow":
-            print("You Win!")
+            print("Congratulations! You Win!")
         elif choice3 == "blue":
-            print('You enter a room of beasts. Game over.')
+            print("You enter a room of beasts. Game Over.")
         elif choice3 == "red":
-            print("It's a room full of fire. Game Over")
+            print("It's a room full of fire. Game Over.")
         else:
-            print("You choose a door that doesn't exist. Game Over")
+            print("You choose a door that doesn't exist. Game Over.")
     else:
         print("You got attacked by an angry trout. Game Over.")
 else:
-    print("You fel into a hole. Game Over.")
-
+    print("You fell into a hole. Game Over.")
